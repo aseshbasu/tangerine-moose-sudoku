a8688772e3866dec86454c455a33c39c8e06a2c6
aseshbasu <61047705+aseshbasu@users.noreply.github.com>
2023-08-14 20:36:49 +0530

    Update board.jsx
    *    can take input, numbers only. letters and other numeric values wont be submitted. check with symbols maybe?
    *    will display alert if atleast one row is valid
    *    will display alert if atleast one row is invalid
    **    column checking not yet done.

--- src/app/components/board.jsx
+++ src/app/components/board.jsx
@@ -1,34 +1,76 @@
 'use client'
 
-import { useState } from 'react';
-import Cell from './cell'
-
-const board = [...Array(9)]
+import React, { useState , useEffect} from 'react';
 
 export default function Board() {
-  
-  const [size, setSize] = useState(9)
+  const [grid, setGrid] = useState(Array.from({ length: 9 }, () => new Array(9).fill(0)));
+
+
+  const solve = () => {
+    console.log(grid)
+    const isValidRow = (row) => {
+      const seen = new Set();
+      for (let num of row) {
+        if (parseInt(num) !== 0 && seen.has(parseInt(num))) {
+          return false;
+        }
+        seen.add(num);
+      }
+      return true;
+    };
+
+    const rowValidities = grid.map((row) => isValidRow(row));
+    const isSudokuValid = rowValidities.every((validity) => validity);
+
+    if (isSudokuValid) {
+      alert('Atleast one row is  valid!');
+    } else {
+      alert('Atleast one row is NOT valid.');
+    }
+  };
+
+  useEffect(() => {
+      // Generate a 9x9 grid with random values from 1 to 9
+      const newGrid = Array.from({ length: 9 }, () =>
+        Array.from({ length: 9 }, () =>'-')
+      );
+      setGrid(newGrid);
+    }, []);
+
+  const handleCellClick = (row, col) => {
+    // Prompt the user to input a number between 1 and 9
+    const userInput = prompt('Enter a number between 1 and 9:');
+    const parsedNumber = parseInt(userInput, 10);
+
+    if (!isNaN(parsedNumber) && parsedNumber >= 1 && parsedNumber <= 9) {
+      const updatedGrid = [...grid];
+      updatedGrid[row][col] = parsedNumber;
+      setGrid(updatedGrid);
+    } else {
+      alert('Invalid input. Please enter a number between 1 and 9.');
+    }
+  };
 
   return (
-    <div className="flex flex-row justify-center items-center">
-      { 
-        board.map( (rKey, cIndex) => {
-          return (
-            <div key={cIndex}>
-                {
-                  board.map( (cKey, rIndex) => {
-                    return (
-                      <div className="" key={rIndex + cIndex}>
-                        <Cell key={rIndex} row={rIndex} col={cIndex}/>
-                      </div>
-                    ) 
-                  })
-                }
-            </div>
-            )
-          })
-        
-      }
+    <>
+    <div className="sudoku-grid">
+      {grid.map((row, rowIndex) =>
+        row.map((cell, colIndex) => (
+          <div
+            key={`${rowIndex}-${colIndex}`}
+            className="sudoku-cell"
+            onClick={() => handleCellClick(rowIndex, colIndex)}
+          >
+            {cell !== 0 ? cell : ''}
+          </div>
+        ))
+      )}
     </div>
-  )
-}
+    <br />
+    
+    <div>
+      <button onClick={solve}>Solve</button>
+    </div>
+    </>
+  );
+};
